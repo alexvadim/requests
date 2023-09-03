9f419003a5c3c40ea0eaba71f0166c2b876b0f2d
alexvadim <131587624+alexvadim@users.noreply.github.com>
2023-09-03 15:39:37 +0300

    Add files via upload

---
+++ test_reqres.py
@@ -0,0 +1,114 @@
+import pytest
+import selenium
+import requests
+
+
+def test_get1():
+    response = requests.post('https://run.mocky.io/v3/64f4ecc6-368e-414e-b6fe-8a059affe1ca')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+def test_get2():
+    response = requests.post('https://run.mocky.io/v3/e08e35df-77a2-436a-9eb5-1dca46e0da00')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+
+def test_get3():
+    response = requests.post('https://run.mocky.io/v3/82aded74-373e-4b9e-b721-ebb938e9ac19')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+
+def test_get4():
+    response = requests.post('https://run.mocky.io/v3/a9446b47-b0a1-454d-b89d-c3b848b7dc7c')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+def test_get5():
+    response = requests.post('https://run.mocky.io/v3/d782a2bf-1055-482a-9e3f-dffe25f023bd')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+def test_get6():
+    response = requests.post('https://run.mocky.io/v3/35344e37-ca0e-41a8-9c87-b13141d3e816')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+
+def test_post7():
+    response = requests.post('https://run.mocky.io/v3/0dd3c4e1-fb37-4348-aa06-7a2afc81c16a')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+def test_put8():
+    response = requests.post('https://run.mocky.io/v3/3a4dea4b-17c2-4c5a-8cb2-556af5c7091b')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+def test_patch9():
+    response = requests.post('https://run.mocky.io/v3/ac1f829d-ffce-49d1-8ce6-9778559c98e5')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+def test_delete10():
+    response = requests.post('https://run.mocky.io/v3/44c84d52-a815-40f6-a47c-1b8782b3de6e')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+def test_post11():
+    response = requests.post('https://run.mocky.io/v3/be338e29-d3dd-4298-924e-5604e8a7b350')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+def test_post12():
+    response = requests.post('https://run.mocky.io/v3/85af46cd-ed99-4c9f-ba22-6e34ea5bb8da')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+def test_post13():
+    response = requests.post('https://run.mocky.io/v3/7fcd8e0c-fd4a-4146-bada-700e8790b69a')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+def test_post14():
+    response = requests.post('https://run.mocky.io/v3/b505a79c-0280-4a09-bba0-b984e1eed188')
+    print(response)
+    print(response.headers)
+    print(response.text)
+
+
+
+test_get1()
+test_get2()
+test_get4()
+test_get5()
+test_get6()
+test_post7()
+test_put8()
+test_patch9()
+test_delete10()
+test_post11()
+test_post12()
+test_post13()
+test_post14()
